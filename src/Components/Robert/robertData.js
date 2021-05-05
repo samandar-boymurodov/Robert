2a5d85648c1a7d9c8a02fc85d27507e0ebadddfc
boymurodovuzcoder <boymurodovuzcoder@gmail.com>
2021-05-06 00:27:41 +0500

    Robert's words are complete

--- src/Components/Robert/robertData.js
+++ src/Components/Robert/robertData.js
@@ -2,38 +2,44 @@ import { winnerStatus } from '../Grid Container/utils/index'
 
 export const RobertData = {
     greeting: {
-        gameStart: ["Hi, my name is Robert",
+        gameStart: ["Hi, my name is Robert 😊",
             "Hello",
             "Hi, Let's play a game"
         ],
         restartGameStart: ["So, Let's move!",
             "This game, I will win You",
-            "You are a good opponent."
+            "I'd like to play with you again",
+            "Let's play again",
+            "You are a good opponent"
         ]
     },
     winner: {
-        [winnerStatus.x]: ["You Won!",
+        [winnerStatus.x]: ["You've Won!",
             "You are Amazing!",
-            "Congratulations!"
+            "Congratulations!",
+            "I've lost the game 😔"
         ],
-        [winnerStatus.y]: ["I won You!",
-            "You shoould try one more",
-            "You lost!"
+        [winnerStatus.y]: ["I've won You! 😎",
+            "You shoould try one more 😉",
+            "You've lost!"
         ],
         [winnerStatus.draw]: ["Congratulations, We are draw",
-            "Weird, We are Draw"
+            "None of us has won 😐"
         ]
     },
     turn: {
         [winnerStatus.y]: ["Let me think...",
             "You are meking me think..",
-            "I think I will lose",
+            "You are doing well",
+            "I am afraid..",
             "You are a great player",
+            "A Good move"
         ],
         [winnerStatus.x]: ["Be quick",
             "Your turn...",
             "Robert wants you to move",
-            "Show your ability!"
+            "Use your head..",
+            "Continue.."
         ]
     }
 }
