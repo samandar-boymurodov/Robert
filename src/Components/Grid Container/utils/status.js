87f71e5c7f7cfcc39f25d30615960277dc3ae1da
boymurodovuzcoder <boymurodovuzcoder@gmail.com>
2021-06-09 14:22:46 +0500

    styling improvement

--- src/Components/Grid Container/utils/status.js
+++ src/Components/Grid Container/utils/status.js
@@ -1,5 +1,5 @@
 export const status = {
-    x: `<span style="color: chocolate;">X</span>`,
-    y: `<span style="color: navy;">Y</span>`,
-    unTouched: "unTouched"
-}
+  x: `<span style="color: chocolate;">X</span>`,
+  y: `<span style="color: navy;">O</span>`,
+  unTouched: "unTouched",
+};
